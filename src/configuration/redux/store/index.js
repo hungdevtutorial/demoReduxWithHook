641653cf8d896b7c69878b671bf4cf892aca78d2
hungphamdinh <hungphamdinh100@gmail.com>
2020-06-02 12:33:14 +0700

    update

--- src/configuration/redux/store/index.js
+++ src/configuration/redux/store/index.js
@@ -3,19 +3,15 @@ import thunkMiddleWare from 'redux-thunk';
 import rootReducer from '../reducer/index'; // rootReducer, that were defined
 // in combineReducer..
 
-export default function configureStore(preloadedState) {
-  return createStore(
-    rootReducer,
-    preloadedState,
-    applyMiddleware(thunkMiddleWare),
-  );
-}
-// In previous section, we defined about actions to handle "what happend",
-// reducer to update new state value...
-// And what is store doing.
-// Simple you can thing the store is bring them together
-// Ok now next step...
+//*** IF YOU WANT TO USE REDUX THUNK, YOU CAN USE THIS */
+// export default function configureStore(preloadedState) {
+//   return createStore(
+//     rootReducer,
+//     preloadedState,
+//     applyMiddleware(thunkMiddleWare),
+//   );
+// }
 
-// Well, i forgive to import middleware to handle asynchronous action
-// Because reducer and redux is only synchronous
-// If we want to use dispatch function we need to import redux thunk or redux sage
+export default function configureStore() {
+  return createStore(rootReducer);
+}
